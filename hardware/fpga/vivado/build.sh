7853ecd52b39ace58987f04ad758798f6894e8d2
Dr-Specimen <jose.j.sarmento@me.com>
2020-11-12 22:49:56 +0000

    make fpga works

--- hardware/fpga/vivado/build.sh
+++ hardware/fpga/vivado/build.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
 TOP_MODULE="iob_knn"
 
 export XILINXPATH=/opt/Xilinx
 export LM_LICENSE_FILE=$LM_LICENSE_FILE:$XILINXPATH/Xilinx.lic
-source /opt/Xilinx/Vivado/settings64.sh
+source /opt/Xilinx/Vivado/2020.1/settings64.sh
 vivado -nojournal -log vivado.log -mode batch -source ../knn.tcl -tclargs "$TOP_MODULE" "$1" "$2" "$3" "$4"
