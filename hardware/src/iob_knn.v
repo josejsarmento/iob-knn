dcd6193e8e29607fbece179751d91b6c03e41ee9
josejsarmento <jose.j.sarmento@me.com>
2021-01-04 23:45:43 +0000

    changed knn core

--- hardware/src/iob_knn.v
+++ hardware/src/iob_knn.v
@@ -7,7 +7,10 @@ module iob_knn
   #(
     parameter ADDR_W = `KNN_ADDR_W, //NODOC Address width
     parameter DATA_W = `DATA_W, //NODOC Data word width
-    parameter WDATA_W = `KNN_WDATA_W //NODOC Data word width on writes
+    parameter WDATA_W = `KNN_WDATA_W, //NODOC Data word width on writes
+    parameter LABELS = `LABELS,
+    parameter K_NEIGHBOURS = `K_NEIGHBOURS,
+    parameter M_POINTS = `M_POINTS
     )
    (
 `include "cpu_nat_s_if.v"
@@ -26,18 +29,56 @@ module iob_knn
    `SIGNAL(write, 1) 
    `COMB write = | wstrb;
 
+   `SIGNAL(dist_calculated, 1)
+   `SIGNAL(KNN_OUT, LABELS*K_NEIGHBOURS*M_POINTS)
+   genvar i, j;
+
+   generate
+      for(i = 0; i < `M_POINTS; i = i + 1) begin
+       for (j = 0; j < `K_NEIGHBOURS; j = j + 1) begin
+        assign KNN_M_K[i*`K_NEIGHBOURS+j] = KNN_OUT[((i+1)*`LABELS*`K_NEIGHBOURS)-1:i*`LABELS*`K_NEIGHBOURS];
+      end
+
+ 	   knn_core knn0
+		 (
+		  .clk(clk),
+		  .rst(rst_int),
+		  .write(write),
+		  .Point_1_Label(KNN_N_LABEL),
+		  .Point_1_X(KNN_N_X),
+		  .Point_1_Y(KNN_N_Y),
+		  .Point_2_X(KNN_M_X[i]),
+		  .Point_2_Y(KNN_M_Y[i]),
+		  .valid(dist_calculated),
+		  .knn_enable(KNN_ENABLE),
+		  .labels(KNN_OUT[((i+1)*`LABELS*`K_NEIGHBOURS)-1:i*`LABELS*`K_NEIGHBOURS])
+		  );
+	end
+   endgenerate
+
+ 	FSM control_unit
+       (
+        .dist_calculated(dist_calculated),
+        .valid(valid),
+        .clk(clk),
+        .knn_enable(KNN_ENABLE),
+        .rst(rst),
+        .wstrb(write)
+       );
+
+
    //
    //BLOCK 64-bit time counter & Free-running 64-bit counter with enable and soft reset capabilities
    //
-   `SIGNAL_OUT(KNN_VALUE, 2*DATA_W)
-   knn_core knn0
-     (
-      .KNN_ENABLE(KNN_ENABLE),
-      .KNN_SAMPLE(KNN_SAMPLE),
-      .KNN_VALUE(KNN_VALUE),
-      .clk(clk),
-      .rst(rst_int)
-      );
+  // `SIGNAL_OUT(KNN_VALUE, 2*DATA_W)
+   //knn_core knn0
+   //  (
+   //   .KNN_ENABLE(KNN_ENABLE),
+   //   .KNN_SAMPLE(KNN_SAMPLE),
+   //   .KNN_VALUE(KNN_VALUE),
+   //   .clk(clk),
+   //   .rst(rst_int)
+   //   );
    
    
    //ready signal   
--- hardware/src/knn.v
+++ hardware/src/knn.v
@@ -3,18 +3,29 @@
 
 module knn_core
   #(
-    parameter DATA_W = 32
+    parameter DATA_W = 32,
+    parameter LABELS = 8,
+    parameter M_POINTS = 4,
+    parameter K_NEIGHBOURS = 10
     )
    (
-    `INPUT(KNN_ENABLE, 1),
+    `INPUT(knn_enable, 1),
     `INPUT(KNN_SAMPLE, 1),
     `OUTPUT(KNN_VALUE, 2*`DATA_W),
     `INPUT(clk, 1),
-    `INPUT(rst, 1)
+    `INPUT(rst, 1),
+    `INPUT(Point_1_X, `DATA_W/2),
+    `INPUT(Point_1_Y, `DATA_W/2),
+    `INPUT(Point_2_X, `DATA_W/2),
+    `INPUT(Point_2_Y, `DATA_W/2),
+    `INPUT(write, 1),
+    `INPUT(Point_1_Label, `LABELS),
+    `INPUT(valid, 1),
+    `OUTPUT(labels, `LABELS*`K_NEIGHBOURS)
     );
 
    `SIGNAL(time_counter, 2*DATA_W)
-   `COUNTER_ARE(clk, rst, KNN_ENABLE, time_counter)
+   `COUNTER_ARE(clk, rst, knn_enable, time_counter)
 
    //time counter register
    `SIGNAL(counter_reg, 2*DATA_W)
@@ -23,6 +34,59 @@ module knn_core
    //always @(negedge clk) if(TIMER_SAMPLE)  counter_reg <= time_counter;
 
    `SIGNAL2OUT(KNN_VALUE, counter_reg)
+
+   `SIGNAL(DIST_OUT, `DATA_W)
+
+
+     `SIGNAL2OUT(labels, DIST_OUT)
+
+     dist_calc dist0
+      (
+      .IN_1_X(Point_1_X),
+      .IN_1_Y(Point_1_Y),
+      .IN_2_X(Point_2_X),
+      .IN_2_Y(Point_2_Y),
+      .DIST_OUT(DIST_OUT)
+      );
+
+    `SIGNAL(write_previous, 1)
+    `SIGNAL(write_val, K_NEIGHBOURS)
+    `SIGNAL(neighbour_dist, DATA_W)
+    `SIGNAL(KNEIGHBOURS_CLASS, LABELS*K_NEIGHBOURS)
+    `SIGNAL(KNEIGHBOURS_DIST, DATA_W*K_NEIGHBOURS)
+
+    `SIGNAL2OUT(labels, KNEIGHBOURS_CLASS)
+
+     genvar i;
+
+
+     neighbour0 neighbour_nr0
+    (
+     .dist_candidate(DIST_OUT),
+     .class_candidate(Point_1_Label),
+     .valid(valid),
+     .rst(rst),
+     .knn_enable(knn_enable),
+     .clk(clk),
+     .neighbour_dist(KNEIGHBOURS_DIST[DATA_W-1:0]),
+     .neighbour_label(KNEIGHBOURS_CLASS[LABELS-1:0])
+     );
+
+     generate
+      for(i = 1; i < K_NEIGHBOURS; i = i+1) begin
+        neighbour neighbour_nr
+        (
+		 .dist_candidate(KNEIGHBOURS_DIST[i*DATA_W-1:(i-1)*DATA_W]),
+		 .class_candidate(KNEIGHBOURS_CLASS[LABELS*i-1:LABELS*(i-1)]),
+		 .valid(valid),
+		 .rst(rst),
+		 .knn_enable(knn_enable),
+		 .clk(clk),
+		 .neighbour_dist(KNEIGHBOURS_DIST[(i+1)*DATA_W-1:i*DATA_W]),
+		 .neighbour_label(KNEIGHBOURS_CLASS[LABELS*(i+1)-1:LABELS*i])
+         );
+      end
+    endgenerate
       
 endmodule
 
